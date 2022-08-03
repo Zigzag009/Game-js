0369fc6300e41c03d97459ed1c3dc3f21b38cdee
Zigzag009 <104829155+Zigzag009@users.noreply.github.com>
2022-08-03 11:36:53 +0300

    game
    my game

---
+++ js/script.js
@@ -0,0 +1,49 @@
+function size(argumen) {
+	return Math.floor(Math.random() * argumen);
+};
+
+function getDistance(event, target) {
+	var diffX = event.offsetX - target.x;
+	var diffY = event.offsetY - target.y;
+	return Math.sqrt((diffX * diffX) + (diffY * diffY));
+};
+
+function getDistanceHint(distance) {
+	if (distance < 10) {
+		return "Згорів нахуй!";
+	} else if (distance < 20) {
+		return "Гаряче";
+	} else if (distance < 40) {
+		return "Жарко";
+	} else if (distance < 80) {
+		return "Тепло";
+	} else if (distance < 160) {
+		return "Холодно";
+	} else if (distance < 320) {
+		return "Дуже холодно";
+	} else {
+		return "Замерз нахуй";
+	}
+};
+
+var width = 400;
+var height = 400;
+var clicks = 0;
+
+var target = {
+	x: size(width),
+	y: size(height)
+};
+
+$("#img").click(function(event) {
+	clicks++;
+
+	var distance = getDistance(event, target);
+	var distanceHint = getDistanceHint(distance);
+
+	$("#distance").text(distanceHint);
+
+	if  (distance < 8) {
+		alert("Ти знайшов скарб! Тобі знадобилося " + clicks + " кліків!");
+	}
+});
